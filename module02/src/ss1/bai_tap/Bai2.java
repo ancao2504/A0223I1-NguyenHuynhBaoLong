6b0ca79b7b0b9aaf6a5f31856c3f1f60acf553a5
long134204 <119986877+long134204@users.noreply.github.com>
2023-06-14 12:00:17 +0700

    Add files via upload

---
+++ module02/src/ss1/bai_tap/Bai2.java
@@ -0,0 +1,16 @@
+package ss1.bai_tap;
+
+import java.util.Scanner;
+
+public class Bai2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        String result;
+
+        if (number <10 && number > 0) {
+            result= ""
+        }
+
+    }
+}
