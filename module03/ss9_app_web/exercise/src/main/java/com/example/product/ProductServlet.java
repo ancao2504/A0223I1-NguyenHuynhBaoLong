5c7ec1d3a7639436767b95f01b9fdebf3648364e
unknown <nguyenhuynhbaolong8@gmail.com>
2023-09-22 18:46:56 +0700

    finish

---
+++ module03/ss9_app_web/exercise/src/main/java/com/example/product/ProductServlet.java
@@ -0,0 +1,26 @@
+package com.example.product;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "ProductServlet", value = "/product-servlet")
+public class ProductServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String productDescription = request.getParameter("productDescription");
+        double price = Double.parseDouble(request.getParameter("price"));
+        System.out.println("---------price-------" + price);
+        double percent = Double.parseDouble(request.getParameter("percent"));
+        double discount = price * percent * 0.01;
+        request.setAttribute("discount", discount);
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("index.jsp");
+        requestDispatcher.forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
